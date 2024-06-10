15a3a6544dafce1a8d515711187cb58921c2b660
JaniaGalang <jania.galang@student.ateneo.edu>
2024-06-10 16:23:10 +0800

    Add files via upload

---
+++ set_1.py
@@ -0,0 +1,14 @@
+def savings(gross_pay, tax_rate, expenses):
+   after_tax_pay = gross_pay * (1-tax_rate) // 1
+   remaining_pay = int(after_tax_pay - expenses)
+   return remaining_pay 
+ 
+def material_waste(total_material, material_units, num_jobs, job_consumption):
+    total_consumed = num_jobs * job_consumption
+    remaining_material = total_material - total_consumed
+    return f"{remaining_material}{material_units}"
+
+def interest (principal, rate, periods):
+    investment = principal * rate * periods
+    final_investment = (principal + investment)//1
+    return int(final_investment)
